503499c6374146248d5e0b942f73ff4eca82a4d7
Aya-oba <mbackeousmane31@gmail.com>
2023-10-19 18:11:09 +0000

    14. Average

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,5 @@
+-- Calcule la moyenne des scores de tous les enregistrements dans
+-- la table second_table de la base de données courante.
+-- Le nom de la colonne de résultat doit être moyen.
+
+SELECT AVG(`score`) AS `average` FROM `second_table`;
