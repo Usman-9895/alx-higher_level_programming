93016c432c6987d3044e8e20d183dec9e240fe0e
Aya-oba <mbackeousmane31@gmail.com>
2023-10-19 18:16:09 +0000

    15. Number by score

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,4 @@
+-- Répertorie tous les enregistrements de la table second_table de la base de données actuelle.
+-- Le résultat doit afficher le score et le nom, classés par score.
+
+SELECT `score`, `name` FROM `second_table` WHERE `name` IS NOT NULL ORDER BY `score` DESC;
