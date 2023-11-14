d5a8f6134f5dd248da771e225a3769acf0238129
Aya-oba <mbackeousmane31@gmail.com>
2023-11-14 21:43:23 +0000

    9

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -3,8 +3,8 @@
 /*
 Écrivez une fonction qui imprime le nombre d'arguments déjà imprimés et la nouvelle valeur de l'argument. (voir exemple ci-dessous)
 
-Prototype : exports.logMe = fonction (élément)
-Format de sortie : <nombre d'arguments déjà imprimés> : <valeur actuelle de l'argument>
+Prototype:exports.logMe = fonction (élément)
+Format de sortie:<nombre d'arguments déjà imprimés>:<valeur actuelle de l'argument>
 */
 
 let count = 0;
