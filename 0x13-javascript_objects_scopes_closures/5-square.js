5ff44e4851d488d38065221e7726edb2d5571075
Aya-oba <mbackeousmane31@gmail.com>
2023-11-14 21:36:52 +0000

    5

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 /*
-Écrivez une classe Square qui définit un carré et hérite de Rectangle de 4-rectangle.js :
+Écrivez une classe Square qui définit un carré et hérite de Rectangle de 4-rectangle.js:
 
 Vous devez utiliser la notation de classe pour définir votre classe et étend
 Le constructeur doit prendre 1 argument : taille
