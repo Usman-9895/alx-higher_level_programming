8cfa41b84bc5f32f4fa028d54fbb2a7be28f5628
Aya-oba <mbackeousmane31@gmail.com>
2023-11-18 12:38:35 +0000

    Fichier 1 modifier

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,32 +1,26 @@
 #!/usr/bin/python3
 """
-Script that lists all values in the `states` table of `hbtn_0e_0_usa`
-where `name` matches the argument `state name searched`.
-
-Arguments:
-    mysql username (str)
-    mysql password (str)
-    database name (str)
-    state name searched (str)
+    lists all states with a name starting with N
+    (upper N) from the database hbtn_0e_0_usa
 """
 
-import sys
+
 import MySQLdb
+from sys import argv
+
 
 if __name__ == "__main__":
-    mySQL_u = sys.argv[1]
-    mySQL_p = sys.argv[2]
-    db_name = sys.argv[3]
-
-    searched_name = sys.argv[4]
-
-    # By default, it will connect to localhost:3306
-    db = MySQLdb.connect(user=mySQL_u, passwd=mySQL_p, db=db_name)
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306, user=argv[1],
+        passwd=argv[2],
+        db=argv[3])
     cur = db.cursor()
-
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id"
-                .format(searched_name))
+    cur.execute("""SELECT id, name FROM states WHERE
+         name COLLATE latin1_general_cs LIKE
+         'N%' ORDER BY id ASC;""")
     rows = cur.fetchall()
-
     for row in rows:
         print(row)
+    cur.close()
+    db.close()
