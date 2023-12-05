c87e09de4e6706ebb67aef75c8d3b34edeea1c31
Aya-oba <mbackeousmane31@gmail.com>
2023-12-05 20:24:35 +0000

    Error code #0

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -6,7 +6,7 @@ from urllib import request, error
 from sys import argv
 
 if __name__ == "__main__":
-    try:
+        try:
             with request.urlopen(argv[1]) as page:
                 print(page.read().decode('utf-8'))
         except error.HTTPError as e:
